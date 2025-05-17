38081742f66b673fe00c47cf314e74a3c50f0a08
lmx <2538381255@qq.com>
2025-05-17 22:42:50 +0800

    bug

--- src/views/feature/component/built_in/loading/index.vue
+++ src/views/feature/component/built_in/loading/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { loadingHide, loadingShow } from 'src/ui/components/KmLoading'
+import { loadingHide, loadingShow } from '@/ui/components/KmLoading'
 
 definePage({
   meta: {
